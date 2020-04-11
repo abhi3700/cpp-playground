468f75e9198345f98ea0c6278be8e9c11413eb94
Abhijit Roy <dalvath3700@gmail.com>
2020-04-11 16:32:08 +0530

    output file added, test checked

--- base/test.cpp
+++ base/test.cpp
@@ -1,19 +1,30 @@
-#include <iostream>		// std::cout
-#include <string>		// std::string, std::stoi
+#include <iostream>
+#include <stdio.h>
+
+#define PI 3.1416f
+
+struct person
+{
+	int age;
+	std::string name;
+};
 
 int main() {
-	std::string my_str_var = "23,56,455";
-	int my_int_var_c, my_int_var_cpp;
+	const char* str = "C";
+	std::string strcpp = "C++";
+	// scanf("Enter the string, %s", str);
+	printf("print the string, %s\n", str);
+	std::cout << strcpp <<std::endl;
+	std::cout << "Hello world" <<std::endl;
+    printf("Hello world\n");
 
-	// C++
-	// my_int_var_cpp = std::stoi(my_str_var);
-	// std::cout << my_int_var_cpp + 5 << '\n';
+    person p;
+    p.age = 19;
+    p.name = "abhfjbd";
 
-	// C
-	const char* my_char_var = my_str_var.c_str();
-	my_int_var_c = atoi(my_char_var);
-	std::cout << my_int_var_c << '\n';
-	std::cout << my_int_var_c + 00005 << '\n';
-	
-	return 0;
+    std::cout << p.name << std::endl;
+
+    return 0;
 }
+
+
