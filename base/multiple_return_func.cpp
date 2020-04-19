ac4c84d9cc036bc39095b10c02fc4ca6f5929f34
Abhijit Roy <dalvath3700@gmail.com>
2020-04-19 15:53:14 +0530

    multiple return types in C++

---
+++ base/multiple_return_func.cpp
@@ -0,0 +1,23 @@
+/*
+	Create a multiple return types function.
+*/
+
+#include <iostream>		// cout
+#include <utility>		// std::pair, std::make_pair
+
+// using namespace std;
+using std::string;
+
+
+std::pair<string, int> set_weekno(string day, int num) {
+	return std::make_pair(day, num);
+}
+
+int main() {
+	std::pair<string, int> a = set_weekno("Sunday", 0);
+
+	std::cout << a.first << std::endl;		// Sunday
+	std::cout << a.second << std::endl;		// 0
+
+	return 0;
+}
