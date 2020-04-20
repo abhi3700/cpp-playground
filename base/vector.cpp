2e4f29f53eb570e954994879b9bd3ec37e8ca49e
Abhijit Roy <dalvath3700@gmail.com>
2020-04-20 22:20:35 +0530

    updated

--- base/vector.cpp
+++ base/vector.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-	std::vector<char> c = {'a', 'b'};
-	for (std::vector<char>::iterator i = c.begin(); i != c.end(); ++i)
+using std::string;
+
+
+void vector_eg() {
+	// std::vector<string> v {"aamir", "hrithik", "family"};
+	std::vector<string> v;
+
+
+	v.emplace_back("aamir");
+	v.emplace_back("hrithik");
+	v.emplace_back("family");
+
+	v.pop_back();	// erase from back
+
+
+	std::cout << "Without Iterator:" << std::endl;
+	for (int i = 0; i < v.size(); ++i)
 	{
-		std::cout << *i << "\n";
+		std::cout << v[i] << std::endl;
 	}
+
+	std::cout << "With Iterator:" << std::endl;
+	for (std::vector<string>::iterator i = v.begin(); i != v.end(); ++i)
+	{
+		std::cout << *i << std::endl;
+	}
+}
+
+
+int main() {
+	vector_eg();
+
+
 	return 0;
 }
