69ba9e2a1d5b5e7a1a77d0022c6c6295ed40ac34
Abhijit Roy <dalvath3700@gmail.com>
2020-04-22 02:28:02 +0530

    test, todo added

--- base/test.cpp
+++ base/test.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
-#include <vector>
 
 using std::string;
-
-
-void vector_eg() {
-	// std::vector<string> v {"aamir", "hrithik", "family"};
-	std::vector<string> v;
-
-
-	v.emplace_back("aamir");
-	v.emplace_back("hrithik");
-	v.emplace_back("family");
-
-	v.pop_back();	// erase from back
-
-
-	std::cout << "Without Iterator:" << std::endl;
-	for (int i = 0; i < v.size(); ++i)
-	{
-		std::cout << v[i] << std::endl;
-	}
-
-	std::cout << "With Iterator:" << std::endl;
-	for (std::vector<string>::iterator i = v.begin(); i != v.end(); ++i)
-	{
-		std::cout << *i << std::endl;
-	}
-}
-
+using std::cin;
 
 int main() {
-	vector_eg();
+	string str;
+	std::cout << "Enter a string" << std::endl;
+	
+	cin >> str;
+
+	// getline(cin, str);
+
+	std::cout << str << std::endl;
 
 
 	return 0;
