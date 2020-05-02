3198213922ef872147fa856d22fe71262c9a2988
Abhijit Roy <dalvath3700@gmail.com>
2020-05-02 13:33:01 +0530

    list_of modified by accessing elements iterating

--- libs/boost/examples/assign/assign_listof_op.cpp
+++ libs/boost/examples/assign/assign_listof_op.cpp
@@ -6,6 +6,7 @@
 #include <boost/assert.hpp>
 #include <list>
 #include <stack>
+#include <iterator>
 
 using std::string;
 using namespace boost::assign;
@@ -14,11 +15,17 @@ int main() {
 	//===============LIST====================================
 	// assign
 	std::list<int> l1 = list_of(1) (21) (34) (55) (67);
+	std::list<int>::iterator it = l1.begin();
 
 	// assert
 	BOOST_ASSERT(l1.size() == 5);
 	BOOST_ASSERT(l1.front() == 1);
 	BOOST_ASSERT(l1.back() == 67);
+	advance(it, 2);		// move the iterator's position from 0 to 2
+	BOOST_ASSERT(*it == 34);
+	advance(it, -1);	// move the iterator's position from 2 to 1
+	BOOST_ASSERT(*it == 21);
+
 
 	// display the output
 	for (std::list<int>::iterator i = l1.begin(); i != l1.end(); ++i)
