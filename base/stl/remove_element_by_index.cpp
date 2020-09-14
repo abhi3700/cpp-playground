0c99f014e8d6dd6926eae7d9ca08a0fa72f4cc41
Abhijit Roy <dalvath3700@gmail.com>
2020-09-15 03:47:09 +0530

    added rand no. generation in index

--- base/stl/remove_element_by_index.cpp
+++ base/stl/remove_element_by_index.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*
+	Erase a vector element by index
+	- the index is generated from random no.
+	- then, the element at that index is removed using iterator method.
 
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <time.h>
 
 int main() {
-	std::vector<int> v {432, 4543, 767, 234, 7676, 878};
+	std::vector<int> v {354, 765, 2334, 878, 545, 35345};
+	srand(time(0));
 
-	int rand_index = 2;
+	int rand_index = rand() % (v.size()-1);
+
+	// original vector
+	for(auto&& i : v) {
+		std::cout << i << ",";
+	}
+	std::cout << "\n";
 
 	v.erase(std::find(v.begin(), v.end(), v[rand_index]));
 
 	for(auto&& i : v) {
-		std::cout << i << "\n";
+		std::cout << i << ",";
 	}
+	std::cout << "\n";
+
 	return 0;
 }
