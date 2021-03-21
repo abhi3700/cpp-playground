3205ad7b2c75aa10f8b584c2b4eb8b5fc8008e76
Abhijit Roy <dalvath3700@gmail.com>
2021-03-21 20:44:38 +0530

    modified bonus_2

--- libs/boost/examples/container/bonus-2_vector_of_maps.cpp
+++ libs/boost/examples/container/bonus-2_vector_of_maps.cpp
@@ -22,6 +22,8 @@ using std::make_pair;
 int main() {
 	vector<map<string, string>> v1{};
 	
+	// =========================================================
+	// Insertion
 	// M-1
 	// map<string, string> m1;
 	// m1.insert(make_pair("symbol_name", "EOS"));
@@ -46,7 +48,26 @@ int main() {
 		make_pair("value", "110000")
 	});
 
+	// =========================================================
+	// Search
+	// search for contract as "tokenfutbol1"
+	auto search_index = 0;
 
+	// M-1: using for loop method
+	for (int i = 0; i < v1.size(); ++i)
+	{
+		if(v1[i]["contract"] == "tokenfutbol1") {
+			search_index = i;
+			break;
+		}
+	}
+
+	std::cout << "the element found at index: " << search_index << "\n";
+
+	// M-2: using iterator method TODO
+
+
+	// =========================================================
 	// Display
 	std::cout << v1[0]["symbol_name"] << "\n";
 	std::cout << v1[0]["symbol_precision"] << "\n";
