33988274a760f2c99e3fa571a2260f1c27fc8dc4
Abhijit Roy <dalvath3700@gmail.com>
2021-03-22 19:32:00 +0530

    map_element_2 added, to find the key & change it's value

--- libs/boost/examples/container/map_element_2.cpp
+++ libs/boost/examples/container/map_element_2.cpp
@@ -21,27 +21,40 @@ int main() {
 	std::map<int, string> m1;
 	// insert(m1) (1, "abhijit") (4, "victor") (3, "vincent") (5, "cat");
 	// m1 = list_of<std::pair<int, string>>(1, "abhijit") (4, "victor") (3, "vincent") (5, "cat");
-	m1 = map_list_of(1, "abhijit") (4, "vincent") (3, "vincent") (5, "cat");
+	m1 = map_list_of(1, "abhijit") (4, "victor") (3, "vincent") (5, "cat");
 
 	// access
 	std::cout << m1[4] << std::endl;
+	std::cout << m1.at(4) << std::endl;		// [RECOMMENDED]]
 
 	auto it = m1.begin();
 	std::cout << (*it).second << std::endl;
 	advance(it, 2);
 	std::cout << (*it).second << std::endl;
 
-
+	// change the value of 2nd itr;
+	auto it_change = m1.begin();
+	advance(it_change, 2);
+	it_change->second = "samaira";
 
 
 	// display
-	for (std::map<int, string>::iterator i = m1.begin(); i != m1.end(); ++i)
+	for (auto itr = m1.begin(); itr != m1.end(); ++itr)
 	{
-		std::cout << (*i).first << ", " << (*i).second << std::endl;
+		// std::cout << (*itr).first << ", " << (*itr).second << std::endl;
+		std::cout << itr->first << ", " << itr->second << std::endl;
 	}
+	std::cout << "----------------------------" << "\n";
 
 	for(auto&& i : m1) {
 		std::cout << i.first << ", " << i.second << std::endl;
+	}
+	std::cout << "----------------------------" << "\n";
+
+	std::cout << m1.size() << "\n";
+	for (auto i=0; i <= m1.size(); ++i)
+	{
+		std::cout << m1.at(i) << ", " << m1.at(i) << std::endl;
 	}
 
 
